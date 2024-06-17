642d81ff16e07e6961e975f38a624971aebd9825
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-06-17 07:04:44 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//*****************************************************************************
+//** 633. Sum of Square Numbers Leetcode                                     **
+//** I did NOT use a hash function to solve this, I went with a zero and a   **
+//** square root of the number to come up with two ends, and try to meet at  **
+//** the solution.  It was a slower solution, but it did work.      -Dan     **
+//*****************************************************************************
+//*****************************************************************************
+
+bool judgeSquareSum(int c) {
+    long a = 0;
+    long b = sqrt(c);
+    while(a <= b)
+    {
+        long s = a*a + b*b;
+        if(s == c)
+        {
+            return true;
+        }
+        if (s < c) 
+        {
+            a++;
+        }
+        else
+        {
+            b--;
+        }
+    }
+   return false;
+}
